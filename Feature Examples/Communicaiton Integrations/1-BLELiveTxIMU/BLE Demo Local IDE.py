ef8c3a46275d90becacfa8edee37c56b90d30fbb
Patrick Cavanaugh <99197953+pcavanaugh04@users.noreply.github.com>
2024-12-29 13:54:49 -0500

    Live Data TX and Recieve tutorial
    Created tutorial for connecting and communicating between a BLE client and server. Added different methods of receiving data to demonstrate strengths and weaknesses of each

--- Feature Examples/Communicaiton Integrations/1-BLELiveTxIMU/BLE Demo Local IDE.py
+++ Feature Examples/Communicaiton Integrations/1-BLELiveTxIMU/BLE Demo Local IDE.py
@@ -1,48 +1,150 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
+Welcome to the SwIMU device tutorial series. This is a series of tutorials
+to get you familiar with a few facets of sports engineering, product design
+and microcontroller programming. This is the first tutorial that uses a
+python editor to implement Bluetooth Low-Energy (BLE) between a local computer
+(client) and the SwIMU device (periphrial). Please complete the first 4 modules
+in the "Hardware Programs" tutorial folder to get familiar with the functions
+of the arduino device before starting this tutorial.
 
-This is a temporary script file.
+This makes use of the bleak library in python, which is an implementation of
+the BLE protocol in python. This provides functions to configure and
+communicate with BLE-endabled devices. In this tutorial we will accomplish the
+following things
+    - Configure our local computer as a BLE client
+    - Scan for BLE Devices in our area
+    - Connect to a BLE-enabled periphrial (SwIMU device)
+    - Recieve live IMU data from our SwIMU via notifications
+    
+The Bleak library utilizes asyncio to ensure waiting functions to not block
+the execution of the main program. There are a few requirements to run a
+program using asyncio in spyder including an async "main" function and nesting.
+This will be notated in the code below. For more info on asyncio, see here ->
+https://realpython.com/async-io-python/
 """
-
-# Bluetooth LE scanner
-# Prints the name and address of every nearby Bluetooth LE device
-
+# Imports
 import asyncio
 import nest_asyncio
+import time
 from bleak import BleakScanner, BleakClient
 
+# Define BLE characteristic. This is a custom generated value that each side of
+# the program uses to create a communication "channel" to transmit specific 
+# types of information. For more information on BLE services and
+# characteristics, see this tutorial ->
+# https://novelbits.io/introduction-to-bluetooth-low-energy-book/
 IMU_DATA_UUID = "550e8401-e29b-41d4-a716-446655440002"
+IMU_REQUEST_UUID = "550e8401-e29b-41d4-a716-446655440001"
+TARGET_DEVICE = "SwIMU"
 
+end_time = 10  # time for our data acquisition to run
 
+# Need this to have Asyncio work in the spyder IDE
 nest_asyncio.apply()
 
+async def tx_IMU_with_read(client: BleakClient):
+    ### ------------------- BLE Read Implementation ------------------ ### 
+    # The BLE Read function involves a call/response between the client and
+    # server, there is time assoicated with this, and therefore the band
+    # width is low, however, the transmission is more reliable
+        
+    # Start a loop to run for 10s to read  the IMU_DATA characteristic
+    start_time = time.perf_counter()
+    run_time = 0
+    data_list = []
+    await client.write_gatt_char(IMU_REQUEST_UUID, b"START")
+    while run_time < end_time:
+        # Read data from the IMU characteristic
+        imu_sensor_values = await client.read_gatt_char(IMU_DATA_UUID)
+        # Decode bytes array into human readable string
+        imu_sensor_values = imu_sensor_values.decode("utf-8") 
+        # print(f"Recieved Data: {imu_sensor_values}")
+        # process the data based on format "time, Ax, Ay, Az, Gx, Gy, Gz"
+        split_line = imu_sensor_values.split(",")
+        imu_data_line = (float(x) for x in split_line)
+        data_list.append(imu_data_line)
+        run_time = time.perf_counter() - start_time
+    await client.write_gatt_char(IMU_REQUEST_UUID, b"END")
+    print("----------------- BLE Read Implementation ---------------")    
+    print(f"Number of data packets recieved in {end_time}s: {len(data_list)}")
+    print(f"Realized Frequency [Hz]: {len(data_list) / run_time}")
+    
+async def tx_IMU_with_notify(client):
+    ### ------------------ BLE Notify Implementation ----------------- ### 
+    # The BLE Notify method involves setting up a callback function to
+    # run whenver new data is written to a characteristic on the perephrial
+    # because there is no call/reponse, there is shorter delay between
+    # instances of the program running
+     
+    # Start a loop to run for 10s to read  the IMU_DATA characteristic
+    start_time = time.perf_counter()
+    run_time = 0
+    data_list = []
+    
+    # define a callback function to process data when it arrives
+    async def handle_IMU_notification(sender, data):            
+        nonlocal data_list
+        # Decode bytes array into human readable string
+        imu_sensor_values = data.decode("utf-8") 
+        # print(f"Recieved Data: {imu_sensor_values}")
+        # process the data based on format "time, Ax, Ay, Az, Gx, Gy, Gz"
+        split_line = imu_sensor_values.split(",")
+        imu_data_line = (float(x) for x in split_line)
+        data_list.append(imu_data_line)
+        
+    # Configure the notification
+    await client.start_notify(IMU_DATA_UUID, handle_IMU_notification)
+    await client.write_gatt_char(IMU_REQUEST_UUID, b"START")
+    await asyncio.sleep(end_time)
+    await client.write_gatt_char(IMU_REQUEST_UUID, b"END")
+    # Because each action is not call/response, we have to setup a
+    # start/end notifcation from the client to ensure we're ready
+
+    print("----------------- BLE Notify Implementation ---------------")    
+    print(f"Number of data packets recieved in {end_time}s: {len(data_list)}")
+    print(f"Realized Frequency [Hz]: {len(data_list) / end_time}")
+    
+# Define the "main" function for Asyncio. 
 async def main():
+    # Scan for ble devices in our proximity
     devices = await BleakScanner.discover(timeout=5)
     address = ""
+    # When scanning is complete, see if our target device name was found
     for device in devices:
         print(device)
-        if "SwIMU" in device.__str__():
+        # If so, extract the BLE address associated with the device. these
+        # adresses are unique to each device and is how we connect/
+        # communicate with them
+        if TARGET_DEVICE in device.__str__():
             address = device.__str__().split(": ")[0]
 
     if not address:
         print("Target Device Not Found!")
         return
     
+    # Use the found address to connect to the device
     async with BleakClient(address, timeout=20) as client:
         print("Device Connected!")
-
-        print(client)
+        
+        # Diagnostic syntax to show the available services and characteristics
+        # on the perephrial
         services = client.services
         for service in services:
             print(f"Serice: {service}")
             for char in service.characteristics:
                 print(f" Characteristic: {char}")
         
-        while True:
-            imu_sensor_values = await client.read_gatt_char(IMU_DATA_UUID)
-            print(imu_sensor_values)
-
+        
+        # We have 2 primary ways to read data from a BLE device, we can read
+        # a characteristic, or setup a notification. There's advantages to
+        # each. comment out one or the other to see differences in the 
+        # data rate
+        
+        #### Comment/Uncomment each one to see the difference in behavior ###
+        # await tx_IMU_with_read(client)
+        await tx_IMU_with_notify(client)
+        
         
 if __name__ == "__main__":
     asyncio.run(main())
--- Feature Examples/Communicaiton Integrations/1-BLELiveTxIMU/XIAO_BLE_IMUReadings_Demo/XIAO_BLE_IMUReadings_Demo.ino
+++ Feature Examples/Communicaiton Integrations/1-BLELiveTxIMU/XIAO_BLE_IMUReadings_Demo/XIAO_BLE_IMUReadings_Demo.ino
@@ -1,140 +1,59 @@
 
-#include <SPI.h>
-#include <SD.h>
+
 #include <ArduinoBLE.h>
 #include <LSM6DS3.h>
 #include <string.h>
 
-// SD Card Setup
-const int chipSelect = 5;
 int currentCount;
 
 // BLE Setup
-const char * deviceServiceUuid = "550e8400-e29b-41d4-a716-446655440000";
+const char * IMUServiceUuid = "550e8400-e29b-41d4-a716-446655440000";
 // Ensure First block of characteristic UUID is incremented +1 of Service UUID
-const char * deviceServiceRequestCharacteristicUuid = "550e8401-e29b-41d4-a716-446655440001";
-const char * deviceServiceResponseCharacteristicUuid = "550e8401-e29b-41d4-a716-446655440002";
+const char * IMURequestCharacteristicUuid = "550e8401-e29b-41d4-a716-446655440001";
+const char * IMUDataCharacteristicUuid = "550e8401-e29b-41d4-a716-446655440002";
 
 // Accelerometer Variables
 unsigned long accelStartMillis;
 unsigned long numSamples = 0;
 float accelTime;
+bool TxActive = false;
 //Create a instance of class LSM6DS3
 LSM6DS3 myIMU(I2C_MODE, 0x6A);    //I2C device address 0x6A
 
 
+// These are BLE services and characteristics. A service is a group of characteristics. 
+// Characteristics are the endpoints between client and server where data is written to/from
+// BLE protocol defines a number of standard services and characteristics, which are 
+// noted in the official documentation. Users can configure their own custom services and
+// Characteristics, but they need to create their own UUIDs
+BLEService imuService(IMUServiceUuid);
+BLEStringCharacteristic imuRequestCharacteristic(IMURequestCharacteristicUuid, BLEWrite, 10);
+BLEStringCharacteristic imuDataCharacteristic(IMUDataCharacteristicUuid, BLERead | BLENotify, 512);
 
-BLEService imuService(deviceServiceUuid);
-BLEStringCharacteristic imuRequestCharacteristic(deviceServiceRequestCharacteristicUuid, BLEWrite, 4);
-BLEStringCharacteristic imuResponseCharacteristic(deviceServiceResponseCharacteristicUuid, BLERead | BLENotify, 512);
 
-void displayDirectory(File dir, int numTabs=0) {
-  while (true) {
-    File entry = dir.openNextFile();
-    if (!entry) break;
-
-    for (int i=0; i < numTabs; i++)
-      Serial.print('\t');
-
-    Serial.print(entry.name());
-    if (entry.isDirectory()) {
-      Serial.println("/");
-      displayDirectory(entry, numTabs + 1);
-    }
-    else {
-      Serial.print("\t\t");
-      Serial.println(entry.size(), DEC);
-    }
-    entry.close();
-  }
-}
-
-String readAccel() {
-
-      // Serial.println("Entering Read Accel Method");
-      accelTime = (float)(millis() - accelStartMillis) / 1000;
-      //Serial.println("Past accelTime Calculation");
-      char accelBuffer[100];
-      float accelX = myIMU.readFloatAccelX();
-      // Serial.println("Read aX");
-      float accelY = myIMU.readFloatAccelY();
-      // Serial.println("Read aY");
-      float accelZ = myIMU.readFloatAccelZ();
-      // Serial.println("Read aZ");
-      float gyroX = myIMU.readFloatGyroX();
-      // Serial.println("Read gX");
-      float gyroY = myIMU.readFloatGyroY();
-      // Serial.println("Read gY");
-      float gyroZ = myIMU.readFloatGyroZ();
-      // Serial.println("Read gZ");
-      sprintf(accelBuffer, "%.5f, %.4f, %.4f, %.4f, %.4f, %.4f, %.4f", accelTime, accelX, accelY, accelZ, gyroX, gyroY, gyroZ);
-      // Serial.print("Constructed Accel Buffer: ");
-      // Serial.println(accelBuffer);
-      // Serial.print("With size of: ");
-      // Serial.println(sizeof(accelBuffer));
-
-      // Serial.print("From inside readAccel function: ");
-      // Serial.println(accelBuffer);
-      return String(accelBuffer);
-      // Serial.println(accelBuffer);
-      // accelDataFile.println(accelBuffer);
-      // numSamples = numSamples + 6;
+// User defined function for reading accellerometer data
+const char* readAccel() {
+// Read a line of values from the accelerometer
+  // Calculate elapsed time
+  accelTime = (float)(millis() - accelStartMillis) / 1000;
+  // Create a buffer to collect the values to transmit
+  static char accelBuffer[100];
+  
+  // Read Accelerometer values
+  float accelX = myIMU.readFloatAccelX();
+  float accelY = myIMU.readFloatAccelY();
+  float accelZ = myIMU.readFloatAccelZ();
+  float gyroX = myIMU.readFloatGyroX();
+  float gyroY = myIMU.readFloatGyroY();
+  float gyroZ = myIMU.readFloatGyroZ();
+  sprintf(accelBuffer, "%.5f, %.4f, %.4f, %.4f, %.4f, %.4f, %.4f", accelTime, accelX, accelY, accelZ, gyroX, gyroY, gyroZ);
+  // Increment a counter to track data rate
+  numSamples++;
+  // Serial.println(accelBuffer);
+  return accelBuffer;
 }
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  while(!Serial);
-  digitalWrite(chipSelect, HIGH);
-  Serial.println("Initializing SD Card...");
-  delay(1000);
-
-  if (!SD.begin(chipSelect)) {
-    Serial.println("Initializing failed!");
-    while (1);
-  }
-
-  else {
-    Serial.println("Initialization done.");
-  }
-
-  File root = SD.open("/");
-  displayDirectory(root);
-  root.close();
-  /*
-  File file = SD.open("/test2.txt");
-  if (file) {
-    while (file.available()) {
-      String line = file.readStringUntil('\n');
-      Serial.println(line);
-    }
-    file.close();
-  }
-  else {
-    Serial.println("Error opening file!");
-  }
-  */
-  /*
-  // Create Accelerometer Data Folder
-  if(SD.mkdir("accelDir")) {
-    Serial.println("Create directory success!");
-  }
-  */
-  // SD.remove("counter.txt");
-  // SD.rmdir("accelDir");
-  
-//  File newCounterFile = SD.open("accelDir/counter.txt", FILE_WRITE);
-//  newCounterFile.print("0");
-//  newCounterFile.close();
-  
-  // Read contents of counter file to determine most recent file
-  File counterFileContents = SD.open("accelDir/counter.txt", FILE_READ);
-  if (counterFileContents) {
-    currentCount = counterFileContents.parseInt();
-    counterFileContents.close();
-  }
-  
-  else Serial.println("Error opening Counter File!");
   
   // Accelerometer Init
   if (myIMU.begin() != 0) {
@@ -144,6 +63,7 @@ void setup() {
     Serial.println("Device OK!");
   }
 
+  // Configure discovery characteristics, this allows us to recongize the device by name
   BLE.setDeviceName("SwIMU");
   BLE.setLocalName("SwIMU");
 
@@ -154,11 +74,9 @@ void setup() {
 
   // broadcast service to central devices for connections
   imuService.addCharacteristic(imuRequestCharacteristic);
-  imuService.addCharacteristic(imuResponseCharacteristic);
+  imuService.addCharacteristic(imuDataCharacteristic);
   BLE.addService(imuService);
   BLE.setAdvertisedService(imuService);
-
-  imuResponseCharacteristic.writeValue("90");
   BLE.advertise();
   Serial.println("BLE device ready to connect");
 }
@@ -175,25 +93,36 @@ void loop() {
     accelStartMillis = millis();
     Serial.println("New Value of Start Millis: ");
     Serial.println(accelStartMillis);
+    Serial.println("Central Connected! Awaiting data request");
     // Delay in connection to allow central time to config services
-    delay(1);
+    delay(100);
   }
-  // String newValues = readAccel();
-  // Serial.print("New Readings from IMU: ");
-  // Serial.println(newValues);
+
   while (central.connected()) {
-    // We might need some way of notifying the device that everything is setup to go.
-    // This prevents the characteristic from updating while its being configured from central
-    // Serial.println("Central Connected!");
-    // If we want to respond to a query, use this if structure a reading from the sensor
-    // if (imuResponseCharacteristic.written()) {}
-    // String newValues = readAccel();
-    String newValues = readAccel();
-    Serial.print("Value from readAccel: ");
-    Serial.println(newValues);
-    imuResponseCharacteristic.setValue(newValues);
+    // start each loop with a check to the response characteristic to see if
+    // weve recieved new instructions from the client
+    if (imuRequestCharacteristic.written()) {
+      String request = String(imuRequestCharacteristic.value());
+      Serial.println("Request Recieved: " + request);
+      // If we've recieved a start command, switch the flag to true
+      if (request.equals("START")) {
+        TxActive = true;
+        numSamples = 0;
+      }
 
-    delay(10);
+      // If we recieve a stop command, switch command to false and calc data rate
+      // of the session
+      else {
+        TxActive = false;
+        float freq = numSamples/accelTime;
+        Serial.println("Realized Data Frequency [Hz]: " + String(freq));
+      }
+    }
 
+    if(TxActive == true) {
+      const char* newValues = readAccel();
+      // Serial.print("Value from readAccel: " + newValues);
+      imuDataCharacteristic.setValue(newValues);
+    }
   }
 }
